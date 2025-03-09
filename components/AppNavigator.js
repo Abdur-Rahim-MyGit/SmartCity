53707feef45daa53c9aeeadeab57267b49899531
Abdur Rahim V A <109742126+Abdur-Rahim-MyGit@users.noreply.github.com>
2025-03-09 06:01:04 +0530

    Update AppNavigator.js

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -9,8 +9,7 @@ const Stack = createStackNavigator();
 export const AppNavigator = () =>  {
   return (
     <Stack.Navigator screenOptions={{headerShown:false}}>
-      <Stack.Screen name="home" component={HomeScreen}
-       />
+      <Stack.Screen name="home" component={HomeScreen} />
       <Stack.Screen name="schedule" component={ScheduleScreen} />
       <Stack.Screen name="payment" component={PaymentScreen} />
     </Stack.Navigator>
